5950eacad01dac9ea580df32ec945d571384f053
RandilT <randiltharusha72@gmail.com>
2023-12-06 00:20:15 +0530

    Make the hero section responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,31 @@ function App() {
   return (
     <>
       <Navbar />
+      <div className="container">
+        <div className="pfp">
+          <img
+            src="https://pics.craiyon.com/2023-10-30/66ff0c95358e458c859ecea5d3764b32.webp"
+            alt=""
+          />
+        </div>
 
-      <div className="button-effect">
-        <button className="glowing-btn">
-          <span className="glowing-txt">
-            <span className="faulty-letter">Iam</span>
-          </span>
-        </button>
-        <br />
-        <button className="glowing-btn">
-          <span className="glowing-txt">
-            <span className="faulty-letter">R</span>ANDIL
-            <span className="faulty-letter">T</span>HARUSHA
-          </span>
-        </button>
-        <div className="hero-text">An Aspiring Fullstack Developer</div>
+        <div className="button-effect">
+          <button className="glowing-btn">
+            <span className="glowing-txt">
+              <span className="faulty-letter">IAM</span>
+            </span>
+          </button>
+          <br />
+          <button className="glowing-btn">
+            <span className="glowing-txt">
+              <span className="faulty-letter">R</span>ANDIL
+              <span className="faulty-letter">T</span>HARUSHA
+            </span>
+          </button>
+          <div className="hero-text">
+            <p>An Aspiring Fullstack Developer</p>
+          </div>
+        </div>
       </div>
     </>
   );
