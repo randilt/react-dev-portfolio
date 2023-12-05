f7f3c4b4d44ffe2cecbebe63099e4270e8906737
RandilT <randiltharusha72@gmail.com>
2023-12-05 23:55:24 +0530

    Finish navbar and create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import "./App.css";
+
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+
+      <div className="button-effect">
+        <button className="glowing-btn">
+          <span className="glowing-txt">
+            <span className="faulty-letter">Iam</span>
+          </span>
+        </button>
+        <br />
+        <button className="glowing-btn">
+          <span className="glowing-txt">
+            <span className="faulty-letter">R</span>ANDIL
+            <span className="faulty-letter">T</span>HARUSHA
+          </span>
+        </button>
+        <div className="hero-text">An Aspiring Fullstack Developer</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/GlowButton.jsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+const GlowButton = () => {
+  <div className="button-effect">
+    <button className="glowing-btn">
+      <span className="glowing-txt">
+        C<span className="faulty-letter">L</span>ICK
+      </span>
+    </button>
+  </div>;
+};
+
+export default GlowButton;
